6cb4a8ac3d3ed0a2c3b9535846cb68ea54867dbb
Kostis Michailidis <45876204+kostismich7@users.noreply.github.com>
2019-03-11 19:42:29 +0200

    Add files via upload

---
+++ paradoteo/xv6/sysproc.c
@@ -0,0 +1,155 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "sem.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+
+int sys_shmget(void)
+{
+  char *c;
+  
+  if(argptr(0,&c,sizeof(sh_key_t)) < 0)
+    return -1;
+  
+  return get_page(c);
+}
+
+int sys_shmrem(void)
+{
+  char *c;
+  
+  if(argptr(0,&c,sizeof(sh_key_t)) < 0)
+    return -1;
+  
+  return remove_page(c);
+}
+
+int sys_sem_init(void)
+{
+  int n;
+  char *c;
+  
+  if(argptr(0,&c,sizeof(struct sem)) < 0 || argint(1,&n) < 0)
+    return -1;
+  init_sem((struct sem*)c,n);
+  return 0;
+}
+
+int sys_sem_up(void)
+{
+  char *c;
+  
+  if(argptr(0,&c,sizeof(struct sem)) < 0)
+    return -1;
+  up_sem((struct sem*)c);
+  return 0;
+}
+
+int sys_sem_down(void)
+{
+  char *c;
+  
+  if(argptr(0,&c,sizeof(struct sem)) < 0)
+    return -1;
+  down_sem((struct sem*)c);
+  return 0;
+}
+
+int sys_nprocs_key(void)
+{
+ 
+  char *c;
+  
+  if(argptr(0,&c,sizeof(sh_key_t)) < 0)
+    return -1;
+
+  return num_procs_own_page_key(c);
+}
